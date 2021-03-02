4139140b023faafce33bc9a0b421369811e7ade8
Alphena <alphena@gmail.com>
2021-03-01 19:57:33 -0700

    added readme and cleaned up code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,3 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -91,36 +76,4 @@ function generatePassword(){
  
     return password.join('');
   }
-
-//return arr[Math.floor(Math.random() * arr.length)];
-
-
-// var number = Math.floor((Math.random() * lowercase.length));
-
-// console.log(lowercase[number])
-
-// var number = Math.floor((Math.random() * uppercase.length));
-
-// console.log(uppercase[number])
-
-// var number = Math.floor((Math.random() * numeric.length));
-
-// console.log(numeric[number])
-
-// var number = Math.floor((Math.random() * special.length));
-
-// console.log(special[number])
-
-// var combo = ()
-
-// console.log(combo)
-
-
-// for slot in array array.length 
-
-// var i 
-
-// for (i = 0; i < special.length; i++){
-  
-// }
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # PasswordGenerator
+
+## Overview
+Secure Password generator that creates a password to the user's specifications
+
+## Preview
+
+![Preview Image](assets/images/Generator.PNG)
+
+## Links
+Visit the site at : 
+
+## Objectives
+Create a functional password generatior meeting criteria:  
+* Creates a password at a length specified by the user between (or at) 8 and 128 characters
+* Password generated using criteria that the user selects between lowercase, uppercase, number, or special characters. 
+
+## Systems Used
+* HTML  
+* CSS
+* Javascript
+
+## Credit 
+HTML and CSS provided to me  
+Javascript written by me
